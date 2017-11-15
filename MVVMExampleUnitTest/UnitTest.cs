4794500a528d08994507d1331e68681010d44b39
Camille Alavarado Plasenscia <bgi_camille@hotmail.com>
2017-11-15 13:03:52 -0700

    First Commit

---
+++ MVVMExampleUnitTest/UnitTest.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MVVMUnitTesting
+{
+    [TestClass]
+    public class UnitTestPerson
+    {
+        [TestMethod]
+        public void TestProperties()
+        {//check to see if properties getters setters work;
+            const String expected = new Random().Next();
+            const Int32 expectedInt = new Random().Next();
+
+
+            var PropName = new Person() { Name = expected };
+            var PropAge = new Person() { Age = expected };
+            var PropProffesion = new Person() { Proffesions = expected };
+            var PropID = new Person() { ID = expected };
+
+            Assert.Equal(expected, PropName.Name);
+            Assert.Equal(expectedInt, PropAge.Age);
+            Assert.Equal(expected, PropProffesion.Proffesions);
+            Assert.Equal(expectedInt, PropID.ID);
+        }
+        public void TestAdd()
+        {
+            //Arrange
+            Person person_ = new Person(aidi_, NameX, AgeX, ProfessionX);
+            var aidi_ = aidi_++;
+            var NameX = "Jordan";
+            var AgeX = 22;
+            var ProfessionX = "Programmer";
+            String personStr = person_.Sentence;
+
+            //Act
+            var personList_.Add(person_);
+            var peopleFound_.Add(personStr);
+
+            //Assert
+            Assert.IsNotNull(person_);
+            Assert.IsNotNull(peopleFound_);
+        }
+
+        public void TestDelete()
+        {
+            //Arrange
+            Person person_ = new Person(aidi_, NameX, AgeX, ProfessionX);
+            var aidi_ = aidi_++;
+            var NameX = "Jordan";
+            var AgeX = 22;
+            var ProfessionX = "Programmer";
+            String personStr = person_.Sentence;
+
+            //Act
+            var personList_.Remove(person_);
+            var peopleFound_.Remove(personStr);
+
+            //Assert
+            Assert.IsNull(person_);
+            Assert.IsNull(peopleFound_);
+        }
+
+        public void TestEdit()
+        {
+            //Arrange
+            Person person_ = new Person(aidi_, NameX, AgeX, ProfessionX);
+            var aidi_ = aidi_++;
+            var NameX = "Jordan";
+            var AgeX = 22;
+            var ProfessionX = "Programmer";
+            String personStr = person_.Sentence;
+
+            //Act
+            var personList_.Remove(person_);
+            var peopleFound_.Remove(personStr);
+
+            //Assert
+            Assert.AreNotEqual(person_, newPerson_);
+            Assert.IsNull(peopleFound_);
+        }
+    }
+}
---
+++ MVVMExampleUnitTest/UnitTestAirPlane.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMUnitTestingAirPlane
+{
+    [TestClass]
+    public class UnitTestAirPlane
+    { 
+        [TestMethod]
+        public void TestProperties()
+        {//check to see if properties getters setters work;
+            const String expected = new Random().Next();
+            const Int32 expectedInt = new Random().Next();
+
+
+            var PropCompanyName = new AirPlane() { CompanyNameModel = expected };
+            var PropTypeOfPlane = new AirPlane() { TypeOfPlaneModel = expected };
+            var PropNumOfPlanes = new AirPlane() { NumOfPlanesModel = expected };
+            var PropiD = new AirPlane() { iD = expected };
+
+            Assert.Equal(expected, PropCompanyName.CompanyNameModel);
+            Assert.Equal(expected, PropTypeOfPlane.TypeOfPlanesModel);
+            Assert.Equal(expectedInt, PropNumOfPlanes.NumOfPlanesModel);
+            Assert.Equal(expectedInt, PropiD.iD);
+
+        }
+    }
+
+
+        public void TestAdd()
+        {
+            //Arrange
+            AirPlane airPlane_ = new AirPlane(aidi_, CompanyName, NumOfPlanes, TypeOfPlanes);
+            var aidi_ = aidi_++;
+            var CompanyName = "American Airlines";
+            var NumOfPlanes = 5;
+            var TypeOfPlanes = "CargoPlane";
+            String AirPlaneStr = airPlane_.Sentence;
+
+            //Act
+            var AirPlaneList_.Add(airPlane_);
+            var AirPlaneFound_.Add(AirPlaneStr);
+
+            //Assert
+            Assert.IsNotNull(AirPlaneList_);
+            Assert.IsNotNull(AirPlaneFound_);
+        }
+
+        public void TestDelete()
+        {
+            //Arrange
+            AirPlane airPlane_ = new AirPlane(aidi_, CompanyName, NumOfPlanes, TypeOfPlanes);
+            var aidi_ = aidi_++;
+            var CompanyName = "American Airlines";
+            var NumOfPlanes = 5;
+            var TypeOfPlanes = "CargoPlane";
+            String AirPlaneStr = airPlane_.Sentence;
+
+            //Act
+            var AirPlaneList_.Remove(airPlane_);
+            var AirPlaneFound_.Remove(AirPlaneStr);
+
+            //Assert
+            Assert.IsNull(AirPlaneList_);
+            Assert.IsNull(AirPlaneFound_);
+        }
+
+        public void TestEdit()
+        {
+            //Arrange
+            AirPlane airPlane_ = new AirPlane(aidi_, CompanyName, NumOfPlanes, TypeOfPlanes);
+            var aidi_ = aidi_++;
+            var CompanyName = "American Airlines";
+            var NumOfPlanes = 5;
+            var TypeOfPlanes = "CargoPlane";
+            String AirPlaneStr = airPlane_.Sentence;
+
+            //Act
+            var personList_.Remove(person_);
+            var peopleFound_.Remove(personStr);
+
+            //Assert
+            Assert.AreNotEqual(person_, newPerson_);
+            Assert.IsNull(peopleFound_);
+        }
+        }
+    }
+}
